59d09e633e1f755c231399b60ce682fa64dcb7a6
markogilvie <mark.ogilvie@crichq.com>
2017-06-24 15:24:35 +1200

    post results on front page

--- OpenHostWebsite/httpdocs/Competitions/Games Of The Future/2017/Notices/Games Of The Future 2017.php
+++ OpenHostWebsite/httpdocs/Competitions/Games Of The Future/2017/Notices/Games Of The Future 2017.php
@@ -10,3 +10,7 @@
 <p>
 	<a target="_blank" href="https://goo.gl/forms/ERrldlfp6rQLbeXl2">Online registrations</a> are open now, registrations close Thursday 15th June. 
 </p>
+<p>
+	<a target="_blank" href="http://www.fencingmidsouth.org.nz/Competitions/Games Of The Future/2017/Results/index.htm">Results are now available</a> . 
+</p>
+
