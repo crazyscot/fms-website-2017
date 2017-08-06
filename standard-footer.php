8bdac9b5aa889c1365dfdbaba7e1a1e97b5111a0
markogilvie <mark.ogilvie@crichq.com>
2017-08-06 16:33:45 +1200

    remove updated_at from footer

--- standard-footer.php
+++ standard-footer.php
@@ -1,5 +1,5 @@
       <footer>
-        <address> Updated: 20 February, 17:53<br>
+        <address><br>
           <br>
           Contact: <a href="mailto:info@fencingmidsouth.org.nz">Fencing Mid
             South</a> </address>
