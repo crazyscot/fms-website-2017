46c6d772e3d80babffbd279836a16b5bb8b76525
markogilvie <mark.ogilvie@crichq.com>
2018-01-15 21:07:36 +1300

    add a comma

--- Notices/calendar2018.php
+++ Notices/calendar2018.php
@@ -5,7 +5,7 @@ sidebar on the left of this page. This link will be available here all year.
 </p>
 <p>
 Note that we are now using the calendar feature of Revolutionise Sport. To get more details
-or register for any event you can see in the calendar click on the event in the calendar. Note
+or register for any event you can see in the calendar, click on the event in the calendar. Note
 that you can view the events in a list format as well as the calendar format; you can filter events
 using the event filters at the top of the page.
 </p>
