905c99d267f61c68c1f8aea35747e6acddeeb460
markogilvie <mark.ogilvie@crichq.com>
2018-01-15 21:05:47 +1300

    more calendar descriptions

--- Notices/calendar2018.php
+++ Notices/calendar2018.php
@@ -1,9 +1,11 @@
 <h2>2018 Calendar</h2>
 <p>The <a href="https://www.revolutionise.com.au/fencingmidsouth/events/calendar/" target="_blank">calendar for 2018</a> 
 is out now. You can use the "MidSouth Calendar" link on the 
-sidebar on the left of this page, which will be available all year.
+sidebar on the left of this page. This link will be available here all year.
 </p>
 <p>
-Note that we are now using the calendar feature of Revolutionise Sport. This will 
-let us link from the calendar directly to the details and registration of each event.
+Note that we are now using the calendar feature of Revolutionise Sport. To get more details
+or register for any event you can see in the calendar click on the event in the calendar. Note
+that you can view the events in a list format as well as the calendar format; you can filter events
+using the event filters at the top of the page.
 </p>
