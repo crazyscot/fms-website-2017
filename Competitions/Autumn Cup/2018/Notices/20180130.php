e6b2e127ee44220ff46fff1c65ade4fd79649418
markogilvie <mark.ogilvie@crichq.com>
2018-01-30 21:20:30 +1300

    fix ref to image

--- Competitions/Autumn Cup/2018/Notices/20180130.php
+++ Competitions/Autumn Cup/2018/Notices/20180130.php
@@ -1,5 +1,5 @@
 <h2><a href="http://www.fencingmidsouth.org.nz/Competitions/Autumn Cup/2018/">Autumn Cup 2018</a></h2>
-<img src="http://www.fencingmidsouth.org.nz/Competitions/Autumn Cup/2018/Notices/AutumnCup2018BIO120.JPG">
+<img src="http://www.fencingmidsouth.org.nz/Competitions/Autumn Cup/2018/Notices/AutumnCup2018BIO120.jpg">
 <p>
 The 2018 season is here! And the first competition of the season, Autumn Cup, is on Sunday 4th March, Foil and Epee, exact format dependent on numbers.
 </p>
