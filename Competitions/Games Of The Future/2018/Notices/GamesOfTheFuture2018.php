baf9a526139aa59b75d581dd5ddc68b196094cbb
markogilvie <mark.ogilvie@crichq.com>
2018-06-17 14:36:25 +1200

    gotf small logo

--- Competitions/Games Of The Future/2018/Notices/GamesOfTheFuture2018.php
+++ Competitions/Games Of The Future/2018/Notices/GamesOfTheFuture2018.php
@@ -1,11 +1,11 @@
-<h2><a href="http://www.fencingmidsouth.org.nz/Competitions/Games Of The Future/2018/">Games of the Future 2017</a></h2>
-<img src="http://www.fencingmidsouth.org.nz/Competitions/Games Of The Future/2018/GOTFLogoHeader.JPG">
+<h2><a href="http://www.fencingmidsouth.org.nz/Competitions/Games Of The Future/2018/">Games of the Future 2018</a></h2>
+<img src="http://www.fencingmidsouth.org.nz/Competitions/Games Of The Future/2018/GOTFLogoHeaderSm.JPG">
 <p>
 	The stars of tomorrow honoring Olympic Day and our Olympic champions! 23rd and 24th June.
 </p>
 <p>
 	Full details, including timetable and gear requirements, are   
-	<a target="_blank" href="http://www.fencingmidsouth.org.nz/Competitions/Games Of The Future/2017/Notices/GamesOfTheFuture2018.pdf">available in the brochure<a>.
+	<a target="_blank" href="http://www.fencingmidsouth.org.nz/Competitions/Games Of The Future/2018/Notices/GamesOfTheFuture2018.pdf">available in the brochure<a>.
 </p>
 <p>
 	<a target="_blank" href="https://goo.gl/forms/jKtyGvHC5Pkgwf3p2">Online registrations</a> are open now, registrations close Thursday 21st June. 
