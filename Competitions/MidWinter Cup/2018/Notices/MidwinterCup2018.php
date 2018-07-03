09053cebd5f2c787ba7bc7de8e8c9c19f3201a58
markogilvie <mark.ogilvie@crichq.com>
2018-07-03 21:29:55 +1200

    fix brochure

--- Competitions/MidWinter Cup/2018/Notices/MidwinterCup2018.php
+++ Competitions/MidWinter Cup/2018/Notices/MidwinterCup2018.php
@@ -4,7 +4,7 @@
 	Due to a scheduling clash, Mid-Winter Cup this year is split across two weekends. 
 	Individual foil and Epee is on 15th July. The teams event is on 22nd July. 
 	Full details, including timetable and gear requirements, are   
-	<a target="_blank" href="http://www.fencingmidsouth.org.nz/Competitions/MidWinter Cup/2017/Notices/Mid Winter Cup 2018.pdf">available in the brochure<a>.
+	<a target="_blank" href="http://www.fencingmidsouth.org.nz/Competitions/MidWinter Cup/2018/Notices/Mid Winter Cup 2018.pdf">available in the brochure<a>.
 </p>
 <p>
 	<a target="_blank" href="https://goo.gl/forms/9DhKdzv3pplEPfgZ2">Online registrations</a> are open now.
