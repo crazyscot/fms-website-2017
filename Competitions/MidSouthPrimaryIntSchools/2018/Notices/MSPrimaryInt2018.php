3e7001fb2b8bcab51b923c122d34f07ab38c71ee
markogilvie <mark.ogilvie@crichq.com>
2018-08-04 19:32:48 +1200

    Change MS Primary Int

--- Competitions/MidSouthPrimaryIntSchools/2018/Notices/MSPrimaryInt2018.php
+++ Competitions/MidSouthPrimaryIntSchools/2018/Notices/MSPrimaryInt2018.php
@@ -1,5 +1,6 @@
 <h2><a href="http://www.fencingmidsouth.org.nz/Competitions/MidSouthPrimaryIntSchools/2018/">MidSouth Primary and Intermediate Schools Competition 2018</a> </h2>
-<p>Saturday 4th August, The Fencing Institute.</p>
+<p>The date of this has changed to Saturday 23rd September at The Fencing Institute. (The change
+is because the venue is required for the Super Rugby final on the orignially scheduled date).</p>
 <p>Please refer to our 
 	<a href= 'https://www.revolutionise.com.au/fencingmidsouth/events/14532/' target="_blank"> events page </a>
 	for full details and registration.
