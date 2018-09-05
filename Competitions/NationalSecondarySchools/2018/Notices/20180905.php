ff1dfa6f299d367c5ebe4b4b288f540db7cf49e7
Mark Ogilvie <markogilvienz@gmail.com>
2018-09-05 21:21:14 +1200

    National Secondary Schools take 1

---
+++ Competitions/NationalSecondarySchools/2018/Notices/20180905.php
@@ -0,0 +1,6 @@
+			<h2><a href="http://www.fencingmidsouth.org.nz/Competitions/NationalSecondarySchools/2018/">National Secondary Schools 2018</a> </h2>
+			<p>National Secondary Schools are in Christchurch this year. Enrolments are closing soon.
+			  <a href= "https://www.revolutionise.com.au/fencingmidsouth/events/14538/" target="_blank"> More information and registration </a> is available.  Live results will be available
+			<a href="http://www.fencingmidsouth.org.nz/Competitions/NationalSecondarySchools/2018/Results" target="_blank">here</a>
+			</p>
+	
