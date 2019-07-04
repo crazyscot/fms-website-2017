41345a1d4ccf1a23965e737a6bb99330a3ad2465
Mark Ogilvie <markogilvienz@gmail.com>
2019-07-04 22:01:44 +1200

    PresCup 2019

---
+++ Competitions/MidSouth Secondary School Champs/MidSouthSecondarySchoolChamps.php
@@ -0,0 +1,5 @@
+<h1>Mid South Secondary School Champs</h1>
+<p>
+	The Mid South Secondary School Champs is open to all secondary school students in the region.
+	It covers all weapons, and includes teams.
+</p>
---
+++ Competitions/MidSouthPrimaryIntSchools/MidSouthPrimaryInt.php
@@ -0,0 +1,6 @@
+<h1>Mid South Primary and Intermediate Schools Competition</h1>
+<p>
+	This tournament  is is for Year 1 to Year 8 students and is aimed at giving our younger age 
+	group fencers competition experience with other fencers from their school and fencing class plus the chance 
+	to meet other fencers from different clubs, classes and schools.				
+</p>
